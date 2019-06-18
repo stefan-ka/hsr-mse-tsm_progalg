74f29cb32bde185b4f55e46220ec6ae9e094cd92
Stefan Kapferer <stefan@kapferer.ch>
2019-06-18 15:47:54 +0200

    Exercise 5: Cannon's matrix multiplication algorithm with non-blocking MPI operations

--- 05_Exercise_MPI/Cannon.cpp
+++ 05_Exercise_MPI/Cannon.cpp
@@ -50,7 +50,7 @@ void cannonBlocking(int* const a, int* const b, int* const c, const int n, const
 	MPI_Sendrecv_replace(a, size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
 	MPI_Cart_shift(comm2D, 0, -myCoords[1], &shiftSrc, &shiftDst);
 	MPI_Sendrecv_replace(b, size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
-
+	
 	// compute ranks of the left and up shifts
 	int leftRank, rightRank, downRank, upRank;
 
@@ -82,6 +82,72 @@ void cannonBlocking(int* const a, int* const b, int* const c, const int n, const
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Cannon's algorithm using non-blocking send and receive operations and Cartesian grid
 void cannonNonBlocking(int* const a, int* const b, int* const c, const int n, const int pSqrt) {
-	// TODO
+	// set up the Cartesian topology with wrapparound connections and rank reordering
+	int dims[] = { pSqrt, pSqrt };	// [y,x]
+	int periods[] = { true, true };
+	MPI_Comm comm2D;
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &comm2D);
+
+	// get rank and coordinates with respect to the new topology
+	int my2Drank;
+	int myCoords[2];	// [y,x]
+
+	MPI_Comm_rank(comm2D, &my2Drank);
+	MPI_Cart_coords(comm2D, my2Drank, 2, myCoords);
+
+	// local buffers
+	const int size = n * n;
+	int *aBuffer[] = { a, new int[size] };
+	int *bBuffer[] = { b, new int[size] };
+	
+	// initialize C
+	memset(c, 0, size * sizeof(int));
+
+	// perform the initial matrix alignment: first for A then for B
+	int shiftSrc, shiftDst;
+
+	MPI_Cart_shift(comm2D, 1, -myCoords[0], &shiftSrc, &shiftDst);
+	MPI_Sendrecv_replace(a, size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
+	MPI_Cart_shift(comm2D, 0, -myCoords[1], &shiftSrc, &shiftDst);
+	MPI_Sendrecv_replace(b, size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
+
+	// compute ranks of the left and up shifts
+	int leftRank, rightRank, downRank, upRank;
+
+	MPI_Cart_shift(comm2D, 1, -1, &rightRank, &leftRank);
+	MPI_Cart_shift(comm2D, 0, -1, &downRank, &upRank);
+
+	MPI_Request requests[4];
+
+	// main computation loop
+	for (int i = 0; i < pSqrt; i++) {
+		// non-blocking shifting
+		MPI_Isend(aBuffer[i & 1], size, MPI_FLOAT, leftRank, 1, comm2D, requests);
+		MPI_Isend(bBuffer[i & 1], size, MPI_FLOAT, upRank, 1, comm2D, requests + 1);
+		MPI_Irecv(aBuffer[(i + 1) & 1], size, MPI_FLOAT, rightRank, 1, comm2D, requests + 2);
+		MPI_Irecv(bBuffer[(i + 1) & 1], size, MPI_FLOAT, downRank, 1, comm2D, requests + 3);
+		
+		// matrix multiplication: cLocal += aLocal * bLocal
+		matMultSeq(aBuffer[i & 1], bBuffer[i & 1], c, n);
+
+		// wait for shifts
+		for (int j = 0; j < 4; j++) {
+			MPI_Wait(requests + j, MPI_STATUSES_IGNORE);
+		}
+	}
+
+	// restore the original distribution of A and B 
+	MPI_Cart_shift(comm2D, 1, myCoords[0], &shiftSrc, &shiftDst);
+	MPI_Sendrecv_replace(aBuffer[(pSqrt - 1) & 1], size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
+	MPI_Cart_shift(comm2D, 0, myCoords[1], &shiftSrc, &shiftDst);
+	MPI_Sendrecv_replace(bBuffer[(pSqrt - 1) & 1], size, MPI_FLOAT, shiftDst, 1, shiftSrc, 1, comm2D, MPI_STATUSES_IGNORE);
+
+	// free up communicator
+	MPI_Comm_free(&comm2D);
+
+	// delete buffers
+	delete[] aBuffer[1];
+	delete[] bBuffer[1];
 }
 
